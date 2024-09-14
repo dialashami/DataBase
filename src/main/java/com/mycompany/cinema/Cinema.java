0d7d28f745ca50ddd5e586576c048e4818b94032
Diala Shami <diala.shamii5@gmail.com>
2024-09-15 00:16:19 +0300

    Add files via upload

---
+++ src/main/java/com/mycompany/cinema/Cinema.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cinema;
+
+/**
+ *
+ * @author asus
+ */
+
+public class Cinema {
+
+    public static void main(String[] args) {
+       f1 f=new f1();
+       f.setVisible(false);
+       
+       fa1 ff=new fa1();
+       ff.setVisible(false);
+       
+       fa2 fff=new fa2();
+       fff.setVisible(false);
+       
+       user_F_1 ffff=new user_F_1();
+       ffff.setVisible(false);
+       
+       user_f_2 fffff=new user_f_2();
+       fffff.setVisible(false);
+       
+      
+    
+       user_f_movie1 fffffff =new user_f_movie1();
+       fffffff.setVisible(false);
+       
+      
+       
+          user_snack  fffffffff =new  user_snack ();
+       fffffffff.setVisible(false);
+       
+        fa_employee  ffffffffff =new  fa_employee ();
+       ffffffffff.setVisible(false);
+       
+        fa_hall_employee  fffffffffff =new  fa_hall_employee ();
+       fffffffffff.setVisible(false);
+       
+       fa_movie ffffffffffff =new  fa_movie ();
+       ffffffffffff.setVisible(false);
+       
+        fa_snack1 fffffffffffff =new   fa_snack1 ();
+       fffffffffffff.setVisible(false);
+       
+       fa_ticket ffffffffffffff =new   fa_ticket ();
+       ffffffffffffff.setVisible(false);
+       
+       fa_hall_movie fffffffffffffff =new   fa_hall_movie ();
+       fffffffffffffff.setVisible(false);
+       
+       fa_hall ffffffffffffffff =new   fa_hall ();
+       ffffffffffffffff.setVisible(true);
+    }
+}
---
+++ src/main/java/com/mycompany/cinema/JFileChooser.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.cinema;
+
+import java.io.File;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+/**
+ *
+ * @author asus
+ */
+class JFileChooser {
+
+    static int APPROVE_OPTION;
+
+    JFileChooser(String property) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    File getSelectedFile() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    void setDialogTitle(String select_a_file_to_send) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    int showOpenDialog(Object object) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    void setFileFilter(FileNameExtensionFilter filter) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
